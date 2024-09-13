eb6ebcb05a76573fe0b8f0d0151a8c46d8a0a238
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-09-13 08:48:22 -0300

    1309

---
+++ clashclash/Views/daily/daily.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace clashclash.Views.daily
+{
+    public class dailyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- clashclash/obj/Debug/net8.0/clashclash.AssemblyInfo.cs
+++ clashclash/obj/Debug/net8.0/clashclash.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clashclash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad7b5efcafacfb1e5f8413a7f49bfcc4abb770f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6914c5382e18876bd382b077ef589a3b6498577f")]
 [assembly: System.Reflection.AssemblyProductAttribute("clashclash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clashclash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
