8dc755381e42404a3e24662deb7c5de82e35ba36
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-09-16 09:42:24 -0300

    cu

---
+++ clashclash/Controllers/DailyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace clashclash.Controllers
+{
+    public class DailyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
