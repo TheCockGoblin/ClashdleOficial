82783a53699c8075ec8d80ae561b90bfd1f1bacf
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-11-18 12:27:21 -0300

    4dia

---
+++ clashclash/Controllers/UnlimitedController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace clashclash.Controllers
+{
+    public class UnlimitedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- clashclash/obj/Debug/net8.0/clashclash.AssemblyInfo.cs
+++ clashclash/obj/Debug/net8.0/clashclash.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clashclash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a239811f0ae159d2496de2a2bebb2f46665f5f81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5320502016f4c0b102966d9a50e673e82f5d9181")]
 [assembly: System.Reflection.AssemblyProductAttribute("clashclash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clashclash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
