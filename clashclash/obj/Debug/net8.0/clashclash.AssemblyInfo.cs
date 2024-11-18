f0d4f59670a866dc12bbde2446ee85205330d00e
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-11-18 09:39:08 -0300

    4dias

--- clashclash/obj/Debug/net8.0/clashclash.AssemblyInfo.cs
+++ clashclash/obj/Debug/net8.0/clashclash.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clashclash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e4fa3666adf1f98e9a7ce920ea1548edbcaa13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a239811f0ae159d2496de2a2bebb2f46665f5f81")]
 [assembly: System.Reflection.AssemblyProductAttribute("clashclash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clashclash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
