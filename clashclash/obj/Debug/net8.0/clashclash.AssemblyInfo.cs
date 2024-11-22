f7695ff24020fb638ac0db517a7bc9f6bc819422
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-11-22 08:41:36 -0300

    final mesmo?

--- clashclash/obj/Debug/net8.0/clashclash.AssemblyInfo.cs
+++ clashclash/obj/Debug/net8.0/clashclash.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clashclash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75320ff6e1a5513c402b64658f1e04a6a5485206")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a690ad9c9d2e217ec82e332c58ce6d8ab1194837")]
 [assembly: System.Reflection.AssemblyProductAttribute("clashclash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clashclash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
